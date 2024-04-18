7529e5439ef291c77afba145c15f076be6213b1a
Lucas Beronne <LBeronne@planexware.com>
2024-04-18 11:29:05 -0300

    Se agregan cursos linkedin

--- src/components/sections/curses.tsx
+++ src/components/sections/curses.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -12,11 +12,34 @@ import {
 import Image from "next/image";
 import cursesImage from "./../../assets/cursesImage.svg";
 import { motion } from "framer-motion";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 
 
 
 const Curses: React.FC = () => {
+    const cursesPerPage = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+
     const curses = [
+        {
+            company: 'Linkedin Learning',
+            position: 'DevOps esencial',
+            date: 'VIRTUAL(04/2024)',
+            description: ''
+        },
+        {
+            company: 'Linkedin Learning',
+            position: 'DevOps esencial',
+            date: 'VIRTUAL(04/2024)',
+            description: ''
+        },
+        {
+            company: 'Linkedin Learning',
+            position: 'Java avanzado 2',
+            date: 'VIRTUAL(04/2024)',
+            description: ''
+        },
         {
             company: 'I.T.B.A. - Instituto Tecnológico',
             position: 'CiberSeguridad y Ethical Hacking',
@@ -43,6 +66,20 @@ const Curses: React.FC = () => {
         }
     ]
 
+    const indexOfLastCurse = currentPage * cursesPerPage;
+    const indexOfFirstCurse = indexOfLastCurse - cursesPerPage;
+    const currentCurses = curses.slice(indexOfFirstCurse, indexOfLastCurse);
+
+    const totalPages = Math.ceil(curses.length / cursesPerPage);
+
+    const nextPage = () => {
+        setCurrentPage(currentPage === totalPages ? currentPage : currentPage + 1);
+    };
+
+    const prevPage = () => {
+        setCurrentPage(currentPage === 1 ? currentPage : currentPage - 1);
+    };
+
     return (
         <div>
             <div>
@@ -56,25 +93,35 @@ const Curses: React.FC = () => {
                 </div>
                 <div className="col-span-1 lg:mr-4">
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                        {curses.map((curse, index) => (
+                        {currentCurses.map((curse, index) => (
                             <motion.div
-                            whileHover={{
-                                scale: 1.05,
-                                transition: { duration: 0.3 },
-                            }}
-                            whileTap={{ scale: 0.9 }} key={index}>
-                            <Card className="h-full flex flex-col justify-between">
+                                whileHover={{
+                                    scale: 1.05,
+                                    transition: { duration: 0.3 },
+                                }}
+                                whileTap={{ scale: 0.9 }}
+                                key={index}
+                            >
+                                <Card className="h-full flex flex-col justify-between">
                                     <CardHeader>
                                         <CardTitle>{curse.position}</CardTitle>
                                         <CardDescription>{curse.company}</CardDescription>
                                         <CardDescription>{curse.date}</CardDescription>
                                     </CardHeader>
-                                    {/* <CardFooter className="flex justify-between">
+                                                                        {/* <CardFooter className="flex justify-between">
                                         <Button>Titulo</Button>
                                     </CardFooter> */}
                                 </Card>
                             </motion.div>
                         ))}
+                    </div>
+                    <div className="mt-4 gap-2 flex justify-center">
+                        <Button variant="outline" onClick={prevPage} disabled={currentPage === 1} className='border-2'>
+                            <IoIosArrowBack />
+                        </Button>
+                        <Button variant="outline" onClick={nextPage} disabled={currentPage === totalPages} className='border-2'>
+                            <IoIosArrowForward />
+                        </Button>
                     </div>
                 </div>
             </div>
